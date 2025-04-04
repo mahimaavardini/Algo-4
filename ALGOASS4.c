9e04048960fe4337caafc334dea89e32967d8bf8
mahimaavardini <mahimaavardinibr@gmail.com>
2025-04-04 13:42:54 -0400

    i made a change

---
+++ ALGOASS4.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+//creating a variable defining the matrix
+#define MaxVertices 50
+//creating the matrix
+int AdjacencyMatrix[MaxVertices][MaxVertices];
+// represents number of vertices 
+int n;
+int numOfnodes;
+int count;
+int source, destination, weight;
+
+//Read graph 
+void ReadGraph(FILE *fp) {
+    fp = fopen("testGraph.txt", "r");
+    fscanf(fp, "%d", &numOfnodes);
+    while (count < numOfnodes && fscanf(fp, "%d %d %d", &source, &destination, &weight) == 3) {
+        
+    
+    }
+}
+
